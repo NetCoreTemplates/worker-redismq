c56fec59a3a5f10e169ba39aa8ed143ac0e8458e
Demis Bellot <demis.bellot@gmail.com>
2021-11-12 02:21:28 +0800

    Upgrade to .NET 6.0

--- MyApp.Tests/MqTest.cs
+++ MyApp.Tests/MqTest.cs
@@ -4,34 +4,33 @@ using ServiceStack.Messaging.Redis;
 using ServiceStack.Redis;
 using MyApp.ServiceModel;
 
-namespace MyApp.Tests
+namespace MyApp.Tests;
+
+public class MqTest
 {
-    public class MqTest
+    private IRedisClientsManager redisManager;
+    private readonly IMessageFactory MqFactory;
+    public MqTest()
     {
-        private IRedisClientsManager redisManager;
-        private readonly IMessageFactory MqFactory;
-        public MqTest()
-        {
-            redisManager = new RedisManagerPool("localhost:6379");
-            MqFactory = new RedisTransientMessageFactory(redisManager);
-        }
+        redisManager = new RedisManagerPool("localhost:6379");
+        MqFactory = new RedisTransientMessageFactory(redisManager);
+    }
 
-        [Test] // requires running Host MQ Server project
-        public void Can_send_Request_Reply_message()
+    [Test] // requires running Host MQ Server project
+    public void Can_send_Request_Reply_message()
+    {
+        using (var mqClient = MqFactory.CreateMessageQueueClient())
         {
-            using (var mqClient = MqFactory.CreateMessageQueueClient())
+            var replyToMq = mqClient.GetTempQueueName();
+
+            mqClient.Publish(new Message<Hello>(new Hello { Name = "MQ Worker" })
             {
-                var replyToMq = mqClient.GetTempQueueName();
+                ReplyTo = replyToMq,
+            });
 
-                mqClient.Publish(new Message<Hello>(new Hello { Name = "MQ Worker" })
-                {
-                    ReplyTo = replyToMq,
-                });
-
-                var responseMsg = mqClient.Get<HelloResponse>(replyToMq);
-                mqClient.Ack(responseMsg);
-                Assert.That(responseMsg.GetBody().Result, Is.EqualTo("Hello, MQ Worker!"));
-            }
+            var responseMsg = mqClient.Get<HelloResponse>(replyToMq);
+            mqClient.Ack(responseMsg);
+            Assert.That(responseMsg.GetBody().Result, Is.EqualTo("Hello, MQ Worker!"));
         }
     }
 }
